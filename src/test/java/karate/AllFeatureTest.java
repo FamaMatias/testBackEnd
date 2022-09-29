b4763c89aea52e73de4bbf8c52f332019a10143a
FamaMatias <matiasfama92@gmail.com>
2022-09-29 18:22:43 -0300

    Create runner for report with cucumber

--- src/test/java/karate/AllFeatureTest.java
+++ src/test/java/karate/AllFeatureTest.java
@@ -5,6 +5,8 @@ import com.intuit.karate.Runner;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
+
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +15,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+
 public class AllFeatureTest {
+
+    @Test
+    public void testParallel(){
+        String karateOutputPath = "target/surefire-reports";
+        Results result = Runner.parallel(getClass(),5,karateOutputPath);
+        generateReport(result.getReportDir());
+        Assertions.assertEquals(0,result.getFailCount());
+    }
+
     public static void generateReport(String karateOutputPath){
 
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
@@ -23,13 +35,5 @@ public class AllFeatureTest {
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
 
-    }
-
-    @Test
-    public void testParallel(){
-        String karateOutputPath = "target/surefire-reports";
-        Results result = Runner.parallel(getClass(),1,karateOutputPath);
-        generateReport(result.getReportDir());
-        Assertions.assertEquals(0,result.getFailCount());
     }
 }
